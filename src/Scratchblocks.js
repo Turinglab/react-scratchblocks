85ba32c608ffee586f6ef6097c8f493eff72b0b8
Henry Miskin <henry.miskin@talktalk.net>
2015-12-18 12:55:57 +0000

    Created jQuery independent scratchblocks

--- src/Scratchblocks.js
+++ src/Scratchblocks.js
@@ -3,18 +3,154 @@
  */
 
 import React from 'react'
-import jQuery from 'jQuery'
-import scratchblocks from 'scratchblocks'
+import sb2 from './scratchblocks2'
 
 class Scratchblocks extends React.Component {
 
-  componentDidMount() {
-    jQuery(React.findDOMNode(this.refs.tooltip)).tooltip();
-  }
 
-  render() {
-    return <div ref="tooltip">Hello Friend</div>
-  }
+    constructor() {
+        super();
+        this.sb2 = new sb2();
+    }
+
+
+    parse(selector, options) {
+        selector = selector || "pre.blocks";
+        options = options || {
+            inline: false,
+        }
+
+        // find elements
+        $(selector).each(function (i, el) {
+            var $el = $(el),
+                $container = $('<div>'),
+                code,
+                scripts,
+                html = $el.html();
+
+            html = html.replace(/<br>\s?|\n|\r\n|\r/ig, '\n');
+            code = $('<pre>' + html + '</pre>').text();
+            if (options.inline) {
+                code = code.replace('\n', '');
+            }
+
+            var scripts = this.sb2.parse_scripts(code);
+
+            $el.text("");
+            $el.append($container);
+            $container.addClass("sb2");
+            if (options.inline) {
+                $container.addClass("inline-block");
+            }
+            for (var i=0; i<scripts.length; i++) {
+                var $script = render_stack(scripts[i]).addClass("script");
+                $container.append($script);
+            }
+        });
+    };
+
+    render_stack(script) {
+        var $script = $(document.createElement("div"));
+        for (var i=0; i<script.length; i++) {
+            var info = script[i];
+            $script.append(render_stack_item(info));
+            if (info.comment !== undefined) {
+                $script.append(render_comment(info));
+            }
+        }
+        return $script;
+    }
+
+
+    render_stack_item(info) {
+        switch (info.type) {
+            case "cwrap":
+                var $cwrap = this.render_stack(info.contents).addClass("cwrap")
+                                .addClass(info.category);
+                if (info.shape === "cap") $cwrap.addClass(info.shape)
+                return $cwrap;
+
+            case "cmouth":
+                return this.render_stack(info.contents).addClass("cmouth")
+                                .addClass(info.category);
+
+            default:
+                return this.render_block(info);
+        }
+    }
+
+    render_comment(info) {
+        var $comment = $(document.createElement("div")).addClass("comment")
+                .append($(document.createElement("div"))
+                .append(document.createTextNode(info.comment.trim() || " ")));
+        if (info.shape) {
+            $comment.addClass("attached");
+            $comment.addClass("to-" + info.shape);
+        }
+        return $comment;
+    }
+
+    render_block(info) {
+        if (!info) return;
+
+        // make DOM element
+        var $block = $(document.createElement("div"));
+        $block.addClass(info.shape);
+        $block.addClass(info.category);
+        if (info.flag) $block.addClass(info.flag);
+
+        // color insert?
+        if (info.shape === "color") {
+            $block.css({"background-color": info.pieces[0]});
+            $block.text(" ");
+            return $block;
+        }
+
+        // ringify?
+        var $ring;
+        if (info.is_ringed) {
+            $ring = $(document.createElement("div")).addClass("ring-inner")
+                               .addClass(info.shape).append($block);
+        }
+        if (info.flag === "ring") {
+            $block.addClass("ring");
+        }
+
+        // empty?
+        if (!info.pieces.length && info.flag !== "cend") {
+            $block.addClass("empty");
+            return $ring || $block;
+        }
+
+        // output text segments & args
+        for (var i=0; i<info.pieces.length; i++) {
+            var piece = info.pieces[i];
+            if (typeof piece === "object") {
+                $block.append(render_block(piece));
+            } else if (piece === "@" && info.image_replacement) {
+                var $image = $("<span>")
+                $image.addClass(info.image_replacement);
+                var $span = $("<span>")
+                $span.text(image_text[info.image_replacement]);
+                $image.append($span);
+                $block.append($image);
+            } else if (/^[▶◀▸◂+]$/.test(piece)) {
+                $block.append(
+                    $(document.createElement("span")).addClass("arrow")
+                        .append(document.createTextNode(piece)));
+            } else {
+                if (!piece) piece = " ";
+                $block.append(document.createTextNode(piece));
+            }
+        }
+
+        return $ring || $block;
+    }
+
+    render() {
+        console.log(this.sb2.parse_scripts('repeat 10\n\tsay [Hello]'))
+        return <div>Hello Friend</div>
+    }
 
 }
 
--- src/scratchblocks2.js
+++ src/scratchblocks2.js
@@ -100,9 +100,14 @@ String.prototype.trimRight = function() {
     return this.replace(/\s+$/, "");
 }
 
+Array.prototype.contains = function(item) {
+    for (var i in this){
+        if (this[i] === item) return true;
+    }
+    return false;
+}
 
-
-var scratchblocks2 = function ($) {
+var scratchblocks2 = function () {
     "use strict";
 
     function assert(bool) {
@@ -638,7 +643,7 @@ var scratchblocks2 = function ($) {
         var func = minify(strip_brackets(args[0]).replace(/ v$/, ""));
         if (func == "e^") func = "e ^";
         if (func == "10^") func = "10 ^";
-        info.category = ($.inArray(func, strings.math) > -1) ? "operators"
+        info.category = ( strings.math.contains(func) > -1) ? "operators"
                                                              : "sensing";
     }
 
@@ -653,7 +658,7 @@ var scratchblocks2 = function ($) {
         // Cap block unless argument is "other scripts in sprite"
         if (!args.length) return;
         var what = minify(strip_brackets(args[0]).replace(/ v$/, ""));
-        info.shape = ($.inArray(what, strings.osis) > -1) ? null
+        info.shape = ( strings.osis.contains(what) > -1) ? null
                                                           : "cap";
     }
 
@@ -870,7 +875,7 @@ var scratchblocks2 = function ($) {
             isablock = true;
         } else {
             shape = get_insert_shape(bracket, code);
-            isablock = $.inArray(shape, ["reporter", "boolean", "stack"]) > -1;
+            isablock =  ["reporter", "boolean", "stack"].contains(shape) > -1;
             if (shape.contains("dropdown")) {
                 code = code.substr(0, code.length - 2);
             }
@@ -960,11 +965,11 @@ var scratchblocks2 = function ($) {
         if (overrides) {
             for (var i=0; i<overrides.length; i++) {
                 var value = overrides[i];
-                if ($.inArray(value, override_categories) > -1) {
+                if ( override_categories.contains(value) > -1) {
                     info.category = value;
-                } else if ($.inArray(value, override_flags) > -1) {
+                } else if ( override_flags.contains(value) > -1) {
                     info.flag = value;
-                } else if ($.inArray(value, override_shapes) > -1) {
+                } else if ( override_shapes.contains(value) > -1) {
                     info.shape = value;
                 }
             }
@@ -1042,7 +1047,7 @@ var scratchblocks2 = function ($) {
         // category hack (DEPRECATED)
         if (comment && info.shape !== "define-hat") {
             var match = /(^| )category=([a-z]+)($| )/.exec(comment);
-            if (match && $.inArray(match[2], override_categories) > -1) {
+            if (match &&  override_categories.contains(match[2]) > -1) {
                 info.category = match[2];
                 comment = comment.replace(match[0], " ").trim();
             }
@@ -1346,152 +1351,7 @@ var scratchblocks2 = function ($) {
 
     sb2.parse_scripts = parse_scripts;
 
-
-
-    /*** Render ***/
-
-    /* Render all matching elements in page to shiny scratch blocks.
-     * Accepts a CSS-style selector as an argument.
-     *
-     *  scratchblocks2.parse("pre.blocks");
-     *
-     * (This should really be called "render_all"...)
-     */
-    sb2.parse = function (selector, options) {
-        selector = selector || "pre.blocks";
-        options = options || {
-            inline: false,
-        }
-
-        // find elements
-        $(selector).each(function (i, el) {
-            var $el = $(el),
-                $container = $('<div>'),
-                code,
-                scripts,
-                html = $el.html();
-
-            html = html.replace(/<br>\s?|\n|\r\n|\r/ig, '\n');
-            code = $('<pre>' + html + '</pre>').text();
-            if (options.inline) {
-                code = code.replace('\n', '');
-            }
-
-            var scripts = parse_scripts(code);
-
-            $el.text("");
-            $el.append($container);
-            $container.addClass("sb2");
-            if (options.inline) {
-                $container.addClass("inline-block");
-            }
-            for (var i=0; i<scripts.length; i++) {
-                var $script = render_stack(scripts[i]).addClass("script");
-                $container.append($script);
-            }
-        });
-    };
-
-    function render_stack(script) {
-        var $script = $(document.createElement("div"));
-        for (var i=0; i<script.length; i++) {
-            var info = script[i];
-            $script.append(render_stack_item(info));
-            if (info.comment !== undefined) {
-                $script.append(render_comment(info));
-            }
-        }
-        return $script;
-    }
-
-    sb2.render_stack = render_stack;
-
-    function render_stack_item(info) {
-        switch (info.type) {
-            case "cwrap":
-                var $cwrap = render_stack(info.contents).addClass("cwrap")
-                                .addClass(info.category);
-                if (info.shape === "cap") $cwrap.addClass(info.shape)
-                return $cwrap;
-
-            case "cmouth":
-                return render_stack(info.contents).addClass("cmouth")
-                                .addClass(info.category);
-
-            default:
-                return render_block(info);
-        }
-    }
-
-    function render_comment(info) {
-        var $comment = $(document.createElement("div")).addClass("comment")
-                .append($(document.createElement("div"))
-                .append(document.createTextNode(info.comment.trim() || " ")));
-        if (info.shape) {
-            $comment.addClass("attached");
-            $comment.addClass("to-" + info.shape);
-        }
-        return $comment;
-    }
-
-    function render_block(info) {
-        if (!info) return;
-
-        // make DOM element
-        var $block = $(document.createElement("div"));
-        $block.addClass(info.shape);
-        $block.addClass(info.category);
-        if (info.flag) $block.addClass(info.flag);
-
-        // color insert?
-        if (info.shape === "color") {
-            $block.css({"background-color": info.pieces[0]});
-            $block.text(" ");
-            return $block;
-        }
-
-        // ringify?
-        var $ring;
-        if (info.is_ringed) {
-            $ring = $(document.createElement("div")).addClass("ring-inner")
-                               .addClass(info.shape).append($block);
-        }
-        if (info.flag === "ring") {
-            $block.addClass("ring");
-        }
-
-        // empty?
-        if (!info.pieces.length && info.flag !== "cend") {
-            $block.addClass("empty");
-            return $ring || $block;
-        }
-
-        // output text segments & args
-        for (var i=0; i<info.pieces.length; i++) {
-            var piece = info.pieces[i];
-            if (typeof piece === "object") {
-                $block.append(render_block(piece));
-            } else if (piece === "@" && info.image_replacement) {
-                var $image = $("<span>")
-                $image.addClass(info.image_replacement);
-                var $span = $("<span>")
-                $span.text(image_text[info.image_replacement]);
-                $image.append($span);
-                $block.append($image);
-            } else if (/^[▶◀▸◂+]$/.test(piece)) {
-                $block.append(
-                    $(document.createElement("span")).addClass("arrow")
-                        .append(document.createTextNode(piece)));
-            } else {
-                if (!piece) piece = " ";
-                $block.append(document.createTextNode(piece));
-            }
-        }
-
-        return $ring || $block;
-    }
-
     return sb2; // export the module
 }
 
-export scratchblocks2;
+export default scratchblocks2;
